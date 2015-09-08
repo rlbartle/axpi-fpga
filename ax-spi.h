bd7a6e61f9aeb4c27cd15e2fc30b6acee20b6dff
rlbartle <robert@axium.co.nz>
2015-09-08 16:37:18 +1200

    Update ax-spi.h

--- ax-spi.h
+++ ax-spi.h
@@ -1,1 +1,28 @@
+/*
+ * Raspberry Pi GPIO - FPGA Interface Driver - SPI registration
+ *
+ * Authored by Robert Bartle, July 2015
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+ 
+#ifndef __AX_SPI_H
+#define __AX_SPI_H
 
+#include <linux/spi/spi.h>
+
+extern int ax_register_spi_driver(struct spi_driver *driver);
+
+#endif /* __AX_SPI_H */
