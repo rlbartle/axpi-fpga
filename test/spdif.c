490e0872fbe7af738d2b374233eedef1d907ad0b
rlbartle <robert@axium.co.nz>
2015-10-06 15:43:17 +1300

    Update spdif.c

--- test/spdif.c
+++ test/spdif.c
@@ -13,12 +13,10 @@
 #include <sys/ioctl.h>
 #include <netinet/in.h>
 #include <unistd.h>
-#include <sys/un.h>
 #include <sys/mman.h>
 #include <errno.h>
 #include <sys/time.h>
 
-#include "constants.h"
 #include "spdif/spdif.h"
 
 #define MMAP_STATUS_AVAILABLE 0		//userspace can write into
@@ -28,103 +26,46 @@
 #define dmb(option) __asm__ __volatile__ ("dmb " #option : : : "memory")
 #define dsb(option) __asm__ __volatile__ ("dsb " #option : : : "memory")
 
-static int read_fd(int unix_domain_socket) {
-	struct msghdr msg;
-	ssize_t n;
+void init() {
+	int i = 0;
+	int err = 0;
+	while ((err = kernel_sockets[i] = open("/dev/ax_fpga", O_CREAT | O_RDWR)) != -1 && ++i < 4);
 
-	struct iovec iov;
-	struct cmsghdr *cmptr = NULL;
-	char ctrl_buf[CMSG_SPACE(sizeof(int))];
-	char data[1];
-
-	memset(&msg, 0, sizeof(struct msghdr));
-	memset(ctrl_buf, 0, CMSG_SPACE(sizeof(int)));
-
-	iov.iov_base = data;
-	iov.iov_len = sizeof(data);
-
-	msg.msg_name = NULL;
-	msg.msg_namelen = 0;
-	msg.msg_control = ctrl_buf;
-	msg.msg_controllen = CMSG_LEN(sizeof(int));
-	msg.msg_iov = &iov;
-	msg.msg_iovlen = 1;
-
-	if ((n = recvmsg(unix_domain_socket, &msg, 0)) > 0) {
-		if ((cmptr = CMSG_FIRSTHDR(&msg)) != NULL
-				&& cmptr->cmsg_len == CMSG_LEN(sizeof(int))) {
-			if (cmptr->cmsg_level != SOL_SOCKET) {
-				printf("control level != SOL_SOCKET\n");
-				return -1;
-			}
-			if (cmptr->cmsg_type != SCM_RIGHTS) {
-				printf("control type != SCM_RIGHTS\n");
-				return -1;
-			}
-			printf("AX_FPGA FD obtained\n");
-			return *CMSG_DATA(cmptr);// for cpp : *((int *) CMSG_DATA(cmptr));
-		}
+	if (err == -1) {
+		perror("spdif init");
+		while (i-- > 0)
+			close(kernel_sockets[i]);
 	}
-	return -1;
 }
 
-static int write_output(int unix_domain_client_socket, int output) {
-	return write(unix_domain_client_socket, &output, sizeof(int));
-}
-
-struct spdif_t *spdif_initialise(u_char output) {
-	struct sockaddr_un addr;
-	int unix_domain_socket;
+struct spdif_t *spdif_initialise(u_int8_t output) {
 
 	struct spdif_t *state = malloc(sizeof(struct spdif_t));
 	state->output = output;
 	state->ring_offset = 0;
 
-	if ((unix_domain_socket = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
-		perror("socket error");
+	state->fpga_fd = kernel_sockets[output];
+	close(unix_domain_socket);
+	char *map;
+
+	/* Map circular ring. */
+	if ((map = mmap(NULL, FPGA_RING_ITEM_SIZE*FPGA_RING_ITEM_COUNT, PROT_READ | PROT_WRITE,
+			MAP_SHARED | MAP_LOCKED | MAP_POPULATE, state->fpga_fd, 0)) == MAP_FAILED &&
+		//if attempting to 'lock' the memory fails, then try again without lock
+		(map = mmap(NULL, FPGA_RING_ITEM_SIZE*FPGA_RING_ITEM_COUNT, PROT_READ | PROT_WRITE,
+			MAP_SHARED, state->fpga_fd, 0)) == MAP_FAILED) {
+		perror("mmap ring buffer");
+		state->ring = NULL;
 		goto ERROR;
-	} else {
-		memset(&addr, 0, sizeof(addr));
-		addr.sun_family = AF_UNIX;
-		addr.sun_path[0] = 0;//make it an abstract path
-		strncpy(addr.sun_path+1, GPIO_UDS_PATH, sizeof(addr.sun_path) - 2);
-
-		if (connect(unix_domain_socket, (struct sockaddr*) &addr, sizeof(addr)) == -1) {
-			perror("connect error");
-			goto ERROR;
-		} else {
-			//connected to the gpio helper process.  Tell the GPIO process what output we want
-			if (write_output(unix_domain_socket, output) == -1) {
-				perror("write_output");
-				close(unix_domain_socket);
-				goto ERROR;
-			} else {
-				//read the result from the unix domain socket
-				state->fpga_fd = read_fd(unix_domain_socket);
-				close(unix_domain_socket);
-				char *map;
-
-				/* Map circular ring. */
-				if ((map = mmap(NULL, FPGA_RING_ITEM_SIZE*FPGA_RING_ITEM_COUNT, PROT_READ | PROT_WRITE,
-						MAP_SHARED | MAP_LOCKED | MAP_POPULATE, state->fpga_fd, 0)) == MAP_FAILED &&
-					//if attempting to 'lock' the memory fails, then try again without lock
-					(map = mmap(NULL, FPGA_RING_ITEM_SIZE*FPGA_RING_ITEM_COUNT, PROT_READ | PROT_WRITE,
-						MAP_SHARED, state->fpga_fd, 0)) == MAP_FAILED) {
-					perror("mmap ring buffer");
-					state->ring = NULL;
-					goto ERROR;
-				}
-
-				// Allocate and initialise the ring buffers
-				state->ring = (struct iovec *) malloc(FPGA_RING_ITEM_COUNT * sizeof(struct iovec));
-				for(unsigned i = 0; i < FPGA_RING_ITEM_COUNT; i++) {
-					state->ring[i].iov_base = map + (i * FPGA_RING_ITEM_SIZE);
-					state->ring[i].iov_len  = FPGA_RING_ITEM_SIZE;
-				}
-			}
-		}
 	}
 
+	// Allocate and initialise the ring buffers
+	state->ring = (struct iovec *) malloc(FPGA_RING_ITEM_COUNT * sizeof(struct iovec));
+	for(unsigned i = 0; i < FPGA_RING_ITEM_COUNT; i++) {
+		state->ring[i].iov_base = map + (i * FPGA_RING_ITEM_SIZE);
+		state->ring[i].iov_len  = FPGA_RING_ITEM_SIZE;
+	}
+	
 #ifdef _AX_LOG_LATENCY_
 	gettimeofday(&state->t, NULL);
 	state->rt = 0;
@@ -169,10 +110,10 @@ int write_fpga_msg(struct spdif_t *state, const char *data, size_t data_len) {
 		header = state->ring[state->ring_offset].iov_base;
 
 		//dsb(osh);
-		volatile u_int status;
-		while ((status = (volatile u_int)(header->status)) != MMAP_STATUS_AVAILABLE) {
+		volatile u_int32_t status;
+		while ((status = (volatile u_int32_t)(header->status)) != MMAP_STATUS_AVAILABLE) {
 			// MMAP_STATUS_AVAILABLE 0		//userspace can write into
-			// MMAP_STATUS_SEND_REQUEST 1	//kernel can read
+			// MMAP_STATUS_SEND_REQUEST 1		//kernel can read
 			// MMAP_STATUS_SENDING 2		//kernel is processing
 
 			//The kernel has started processing the data, now waiting on the DMA transfer to complete.
@@ -192,7 +133,6 @@ int write_fpga_msg(struct spdif_t *state, const char *data, size_t data_len) {
 				++state->bna_c;
 //				printf("BNA: ring_offset = %d status:%d poll_events:%d src_data_len:%d remains:%d\n",
 //						state->ring_offset, status, p, data_len, remains);
-//				dsb(osh);
 //				errno = 0;
 //				return 0;
 			}
@@ -245,14 +185,9 @@ int write_fpga_msg(struct spdif_t *state, const char *data, size_t data_len) {
 		}
 
 		//XXX! if the number of iterations is equal to the size of the ring, then a blocking write must be made!!
-		//I don't think it is possible for the caller (spotify_integration) to buffer that much though so just watch it..
-		//16 * 4076 = 65216 bytes.  Max buffer in one-shot is 32768
+		//At the moment it is not possible for the caller to buffer that much though so just watch it..
 	}
 
-	//XXX! sometimes the latencies logged are really big and kernel module is starved of data.  where is the latency coming
-	//from??  Test the time to execute various functions especially system calls like the write below..
-
-	//MSG_DONTWAIT
 	state->write_data_mode[0] = 0;
 	if (write(state->fpga_fd, state->write_data_mode, 1) < 0) {
 		perror("fpga write");
@@ -275,7 +210,7 @@ int write_fpga_msg(struct spdif_t *state, const char *data, size_t data_len) {
 	return 0;
 }
 
-int pause_fpga_msg(struct spdif_t *state, u_char param) {
+int pause_fpga_msg(struct spdif_t *state, u_int8_t param) {
 	state->write_data_mode[0] = 1;
 	state->write_data_mode[1] = param;
 	if (write(state->fpga_fd, state->write_data_mode, 2) < 0) {
